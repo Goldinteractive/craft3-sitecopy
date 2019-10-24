02945dbf77eb9cff044bdb70d6446e294d03d10d
Laurence Cope <info@amitywebsolutions.co.uk>
2019-10-24 18:02:21 +0100

    Update for Commerce Products

--- src/services/SiteCopy.php
+++ src/services/SiteCopy.php
@@ -10,6 +10,7 @@ use Craft;
 use craft\base\Component;
 use craft\base\Element;
 use craft\elements\Entry;
+use craft\commerce\elements\Product;
 use craft\events\ElementEvent;
 use craft\models\Site;
 use yii\base\Event;
@@ -74,10 +75,11 @@ class SiteCopy extends Component
     public function getSiteInputOptions(array $sites = [], $exclude = [])
     {
         $sites = $sites ?: Craft::$app->getSites()->getAllSites();
+
         $sites = array_map(
             function ($site) use ($exclude) {
                 if (!$site instanceof Site) {
-                    $siteId = $site['siteId'] ?? null;
+                    $siteId = $site['siteId'] ?? $site ?? null;
                     if ($siteId !== null) {
                         $site = Craft::$app->sites->getSiteById($siteId);
                     }
@@ -110,7 +112,7 @@ class SiteCopy extends Component
         /** @var Entry $entry */
         $entry = $event->element;
 
-        if (!$entry instanceof Entry) {
+        if (!$entry instanceof Entry && !$entry instanceof Product) {
             return;
         }
 
@@ -133,13 +135,20 @@ class SiteCopy extends Component
             $targets = [$targets];
         }
 
+
+
         foreach ($supportedSites as $supportedSite) {
             $siteId = $supportedSite['siteId'];
+            if(!$siteId)
+            {
+            	$siteId = $supportedSite; // For Products as no siteId key exists
+            }
             $siteElement = Craft::$app->elements->getElementById(
                 $entry->id,
                 null,
                 $siteId
             );
+            
             $matchingTarget = $targets === '*' || in_array($siteId, $targets);
 
             if ($siteElement && $matchingTarget && $entry->siteId !== $siteId) {
@@ -215,6 +224,66 @@ class SiteCopy extends Component
         ];
     }
 
+
+	public function handleSiteCopyActiveStateProduct(craft\commerce\elements\Product $element)
+    {
+        $siteCopyEnabled = false;
+        $selectedSite = null;
+
+        $settings = $this->getCombinedSettings();
+
+        foreach ($settings['settings'] as $setting) {
+            $criteriaField = $setting[0] ?? null;
+            $operator = $setting[1] ?? null;
+            $value = $setting[2] ?? null;
+            $sourceId = $setting[3] ?? null;
+            $targetId = $setting[4] ?? null;
+
+            if (!empty($criteriaField) && !empty($operator) && !empty($value) && !empty($sourceId) && !empty($targetId)) {
+                if (($sourceId != '*' && (int)$sourceId != $element->siteId) || ($criteriaField !== 'typeHandle' && !$element->hasProperty($criteriaField))) {
+                    continue;
+                }
+
+                $checkFrom = false;
+
+                if ($criteriaField === 'id') {
+                    $checkFrom = $element->id;
+                } elseif (isset($element[$criteriaField]['handle'])) {
+                    $checkFrom = $element[$criteriaField]['handle'];
+                }
+
+                $check = false;
+
+                if ($operator === 'eq') {
+                    $check = $checkFrom == $value;
+                } elseif ($operator === 'neq') {
+                    $check = $checkFrom != $value;
+                }
+
+                if ($check && (int)$targetId !== $element->siteId) {
+                    $siteCopyEnabled = true;
+                    $selectedSite = (int)$targetId;
+
+                    if ($settings['method'] == 'or') {
+                        break;
+                    }
+                } elseif ($settings['method'] == 'and' && (int)$targetId !== $element->siteId) {
+                    // check failed, revert values to default
+                    $siteCopyEnabled = false;
+                    $selectedSite = null;
+
+                    break;
+                }
+            }
+        }
+
+        return [
+            'siteCopyEnabled' => $siteCopyEnabled,
+            'selectedSite'    => $selectedSite,
+        ];
+    }
+    
+    
     /**
      * @return array
      */
