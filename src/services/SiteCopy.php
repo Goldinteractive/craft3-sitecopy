329ce65eacb7bf3f46044e89b85fe8a0f1a2a2f0
Manuel Mumenthaler <mum@binaryone.ch>
2020-07-22 14:48:33 +0200

    Change OR  to keep looking after match

--- src/services/SiteCopy.php
+++ src/services/SiteCopy.php
@@ -292,9 +292,6 @@ class SiteCopy extends Component
                     $siteCopyEnabled = true;
                     $selectedSites[] = (int)$targetId;
 
-                    if ($settings['method'] == 'or') {
-                        break;
-                    }
                 } elseif ($settings['method'] == 'and' && (int)$targetId !== $element->siteId) {
                     // check failed, revert values to default
                     $siteCopyEnabled = false;
